0e134234b9248f464924d9ade3310d9c22fbce13
GyovanyFrancini <gyovanyfrancini921@gmail.com>
2026-03-29 16:15:12 -0300

    Create AnalistaFinanceiro.tsx for financial analysis features

---
+++ src/pages/AnalistaFinanceiro.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AnalistaFinanceiro = () => {
+    return (
+        <div>
+            <h1>Financial Analysis Features</h1>
+            {/* Add your financial analysis features here */}
+        </div>
+    );
+};
+
+export default AnalistaFinanceiro;
